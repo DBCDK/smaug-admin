493fcf088b1fd4e37c2313799018b9e0a1fb42a9
Finn Stausgaard <fvs@dbc.dk>
2020-04-24 14:52:46 +0200

    lint

--- src/utils/test.util.js
+++ src/utils/test.util.js
@@ -5,7 +5,7 @@
 
 import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()});
 
 require('babel-register')();
 
