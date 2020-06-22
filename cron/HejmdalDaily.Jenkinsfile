bc5385ef0e1d598728f29d20045400a95c6279c4
Finn Stausgaard <fvs@dbc.dk>
2020-06-22 11:22:34 +0200

    Smaug 268 create stat (#43)
    * SMAUG 268. Pull stats from elk
    
    * Trying to pull credentials
    
    * Trying to pull credentials
    
    * Trying to pull credentials
    
    * Trying to pull credentials
    
    * Trying to pull credentials
    
    * Trying to pull credentials
    
    * Remove bug
    
    * Add MONTHLY env
    
    * Parameters as strings
    
    * Parameters as strings
    
    * Only master branch should push to artifactory
    
    * rewrap
    
    * Add job for /introspection endpoint
    
    * Formatting and only push for master branch
    
    * Test for master
    
    * Test for master
    
    * Test for master
    
    * Push for all branches
    
    * And correct artifact filename
    
    * Typo
    
    * Typo
    
    * Collecting endpoints in one file
    
    * Collecting endpoints in one file
    
    * Add endpoints
    
    * Check for empty string
    
    * Some doc
    
    * Only master should archive
    
    * No need for dbc-logger

---
+++ cron/HejmdalDaily.Jenkinsfile
@@ -0,0 +1,66 @@
+#!groovy
+
+pipeline {
+  agent {
+    label "devel10"
+  }
+  triggers {
+    cron("02 02 * * *")
+  }
+  environment {
+    MONTHLY = " "
+    STAT_FILE = "hejmdal_daily.json"
+    STAT_FILTER = '{"app":"hejmdal", "level":"INFO"}'
+    STAT_ENDPOINT = '{"baseUrl":["/login","/introspection"]}'
+    ELK_CREDENTIALS = credentials('elk_user');
+    ELK_URI = "https://${ELK_CREDENTIALS}@elk.dbc.dk:9100/k8s-frontend-prod-*"
+    ARTIFACTORY_FE_GENERIC = "https://artifactory.dbc.dk/artifactory/fe-generic/metakompasset/"
+    ARTIFACTORY_LOGIN = credentials("artifactory_login")
+  }
+  stages {
+    stage('Install dependencies') {
+      steps { script {
+        sh 'cd cron; npm install;'
+      } }
+    }
+    stage('Create stat files from elk') {
+      steps { script {
+        sh "rm -f ${STAT_FILE}"
+        sh "node cron/fetch_statistics.js '${MONTHLY}' -h '${ELK_URI}' -f '${STAT_FILTER}' -e '${STAT_ENDPOINT}' -o '${STAT_FILE}'"
+      } }
+    }
+  }
+  post {
+    failure {
+      when {branch "master"} {
+        script {
+          slackSend(channel: 'fe-drift',
+            color: 'warning',
+            message: "${env.JOB_NAME} #${env.BUILD_NUMBER} failed and needs attention: ${env.BUILD_URL}",
+            tokenCredentialId: 'slack-global-integration-token')
+        }
+      }
+    }
+    success {
+      script {
+        if ("${env.BRANCH_NAME}" == 'master') {
+          sh "echo archive ${STAT_FILE}"
+          archiveArtifacts "${STAT_FILE}"
+          sh "echo push to ${ARTIFACTORY_FE_GENERIC}${STAT_FILE}"
+          sh "curl -u ${ARTIFACTORY_LOGIN} -T ${STAT_FILE} ${ARTIFACTORY_FE_GENERIC}${STAT_FILE}"
+          slackSend(channel: 'fe-drift',
+            color: 'good',
+            message: "${env.JOB_NAME} #${env.BUILD_NUMBER} completed, and pushed ${STAT_FILE} to ${ARTIFACTORY_FE_GENERIC}",
+            tokenCredentialId: 'slack-global-integration-token')
+        }
+      }
+    }
+    fixed {
+      slackSend(channel: 'fe-drift',
+        color: 'good',
+        message: "${env.JOB_NAME} #${env.BUILD_NUMBER} back to normal: ${env.BUILD_URL}",
+        tokenCredentialId: 'slack-global-integration-token')
+
+    }
+  }
+}
---
+++ cron/fetch_statistics.js
@@ -0,0 +1,188 @@
+/**
+ * @file
+ *
+ * Produce stats for one or more endpoints, using aggregations:
+ * https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations.html
+ * https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-terms-aggregation.html
+ *
+ * -f (filter) is used to select the application and log-level
+ * -e (endpoint) is used to specify the field containing the endpoint(s) and the endpoint(s) to extract stats from
+ *
+ */
+
+const getopts = require('getopts');
+const fs = require('fs');
+const ElasticSearch = require('elasticsearch');
+
+const hourSum = {
+  "date_histogram": {
+    "field": "timestamp",
+    "calendar_interval": "1h",
+    "time_zone": "Europe/Copenhagen",
+    "min_doc_count": 0
+  }
+}
+const daySum = {
+  "date_histogram": {
+    "field": "timestamp",
+    "calendar_interval": "1d",
+    "time_zone": "Europe/Copenhagen",
+    "min_doc_count": 0
+  }
+}
+const search = {
+  size: 0,
+  body: {
+    "query": {bool: { filter: {}}},
+    "aggs": {"hoursum": hourSum, "daysum": daySum}
+  }
+}
+const now = new Date();
+
+/* -- main ----------------------------------------------------------------------------------- */
+
+const [host, filters, endpoints, outfile, monthly] = getFileInfoOrDie(process.argv.slice(2));
+try {
+  oFilters = JSON.parse(filters);
+} catch (e) {
+  usage('filter(s) should be valid json\n - ' + filters + '\n - ' + e.message);
+}
+try {
+  oEndpoints = JSON.parse(endpoints);
+} catch (e) {
+  usage('endpoint(s) should be valid json\n - ' + endpoints + '\n - ' + e.message);
+}
+console.log('Start at', now);
+client = new ElasticSearch.Client({host: host});
+
+if (monthly) {
+  console.log('Get monthly stats');
+  search.body.query.bool.filter = setQueryFilter(oFilters, now.toISOString().slice(0,8) + '01', now.toISOString().slice(0,10));
+}
+else {
+  console.log('Get last 30 days');
+  search.body.query.bool.filter = setQueryFilter(oFilters, "2000-01-01", "9999-12-31");
+}
+console.log('Search:', JSON.stringify(search, null, 2));
+elkSearch(search, oEndpoints).then(function(resp){
+  const res = Object.assign({created: now}, {filter: oFilters}, {endpoint: resp});
+  writeFile(outfile, JSON.stringify(res, null, 2));
+  console.log('Exit at', new Date());
+});
+
+/* -- private ----------------------------------------------------------------------------------- */
+/**
+ * Loop endpoint(s) and collect stats for each of them
+ *
+ * @param search
+ * @param endpoints
+ * @returns {Promise<{}>}
+ */
+async function elkSearch (search, endpoints) {
+  const resp = {};
+  for (const field in endpoints) {
+    for (const ep in endpoints[field]) {
+      const endpoint = endpoints[field][ep];
+      resp[endpoint] = {};
+      let thisSearch = JSON.parse(JSON.stringify(search));
+      thisSearch.body.query.bool.filter.push({"match_phrase": {[field]: endpoint}});
+      const elkResponse = await client.search(thisSearch);
+      Object.keys(elkResponse.aggregations).forEach(sum => {
+        resp[endpoint][sum] = parseAggr(elkResponse.aggregations[sum]);
+      })
+    }
+  }
+  return resp;
+}
+
+/**
+ *
+ * @param aggr
+ * @returns {[]}
+ */
+function parseAggr(aggr) {
+  const res = [];
+  aggr.buckets.forEach(obj => {
+    date = new Date(obj.key_as_string);
+    res.push({date: date.toISOString(), count: obj.doc_count});
+  });
+  return res;
+}
+
+/**
+ *
+ * @param filters
+ * @param from
+ * @param to
+ * @returns {[]}
+ */
+function setQueryFilter(filters, from, to) {
+  const filter = [];
+  filter.push({range: {timestamp:{gte:from, lte:to, format:"strict_date_optional_time"}}});
+  Object.keys(filters).forEach(val => {
+    filter.push({match_phrase:{[val]:filters[val]}})
+  })
+  return filter;
+}
+
+/**
+ *
+ * @param fileName
+ * @param buffer
+ */
+function writeFile(fileName, buffer) {
+  try {
+    return fs.writeFileSync(fileName, buffer);
+  } catch (err) {
+    usage('Cannot write ' + fileName);
+  }
+}
+
+/**
+ *
+ * @param args
+ * @returns {any[]}
+ */
+function getFileInfoOrDie(args) {
+  const options = getopts(args, {
+    alias: {
+      endpoint: 'e',
+      filter: 'f',
+      host: 'h',
+      output: 'o',
+      monthly: 'm'
+    }
+  });
+  if (!options['h']) {
+    usage('missing host');
+  }
+  if (!options['o']) {
+    usage('missing fileinfo');
+  }
+  if (!options['f']) {
+    usage('missing filter(s)');
+  }
+  if (!options['e']) {
+    usage('missing endpoint(s)');
+  }
+  return [options['h'], options['f'], options['e'], options['o'], options['m']];
+}
+
+/**
+ *
+ * @param error
+ */
+function usage(error) {
+  const scriptName = __filename.split(/[\\/]/).pop();
+  const errorTxt = error ? '\nError: ' + error + '\n\n' : '';
+  console.log(
+    'Create a json file with day and hours sums for last 30-ish days.\n' +
+    'Option -m will only produce sums for the current month (normally used on the last day of each month)' +
+    '\n%sUsage \n %s [options]\n\n' +
+    'Options:\n' +
+    ' -h [ElasticSearch host] -f [filter(s)] -e [endpoint(s)] -o  output [json-file] -m\n' +
+    '\nExample:\n' +
+    scriptName + ' -h http://elk.dk -f \'{"app": "my_app", "level": "INFO"}\' -e \'{"endpointName": ["endp-1", endp-2"]}\' -o myFile.json',errorTxt, scriptName);
+  process.exit(1);
+}
+
